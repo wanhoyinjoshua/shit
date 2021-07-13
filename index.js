8741fd81ff36a0f4e3b3aaff84e9e59a08ac9e5d
joshua <wanhoyinsjoahu@yahoo.com>
2021-07-13 21:21:37 +1000

    hhi

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ var store = new MongoDBStore({
 
 
 //parsing http responses 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
